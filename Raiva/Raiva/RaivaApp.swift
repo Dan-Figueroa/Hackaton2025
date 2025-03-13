fc51c5b99eab6360961d09d1953fb86031bc0791
JesusOrtega07 <jesus.ortega07@unach.mx>
2025-03-13 04:11:12 -0600

    Raiva project created

---
+++ Raiva/Raiva/RaivaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RaivaApp.swift
+//  Raiva
+//
+//  Created by Jesus Ortega on 13/03/25.
+//
+
+import SwiftUI
+
+@main
+struct RaivaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
