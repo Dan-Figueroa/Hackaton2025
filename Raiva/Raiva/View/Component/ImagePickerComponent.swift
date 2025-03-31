f3e9fa14af5856c7d5ca599805f81066cc801d2c
Dan Figueroa <danfigueroa@Dans-MacBook-Air.local>
2025-03-31 02:58:13 -0600

    created view creatcommunity, his viewmodel and imagepickercomponent

---
+++ Raiva/Raiva/View/Component/ImagePickerComponent.swift
@@ -0,0 +1,49 @@
+//
+//  ImagePickerComponent.swift
+//  Raiva
+//
+//  Created by Dan Figueroa on 31/03/25.
+//
+
+import SwiftUI
+
+struct ImagePickerComponent: View {
+    @Binding var selectedImage: String
+    let availableImages: [String]
+    @Environment(\.dismiss) var dismiss
+
+    var body: some View {
+        VStack {
+            Text("Selecciona una foto")
+                .font(.custom("Gagalin", size: 28))
+                .foregroundColor(.beige)
+                .padding()
+
+            ScrollView {
+                LazyVGrid(columns: [GridItem(.flexible()), GridItem(.flexible())], spacing: 20) {
+                    ForEach(availableImages, id: \.self) { imageName in
+                        Button(action: {
+                            selectedImage = imageName
+                            dismiss()
+                        }) {
+                            Image(imageName)
+                                .resizable()
+                                .scaledToFill()
+                                .frame(width: 150, height: 150)
+                                .clipShape(Circle())
+                                .overlay(
+                                    Circle()
+                                        .stroke(selectedImage == imageName ? Color.arena : Color.clear, lineWidth: 4)
+                                )
+                        }
+                        .buttonStyle(PlainButtonStyle())
+                    }
+                }
+                .padding()
+            }
+        }
+        .frame(minWidth: 600, minHeight: 500)
+        .background(Color.verdeBosque)
+    }
+}
+//LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))], spacing: 20) 3 columnas
--- Raiva/Raiva/View/CreateComunity.swift
+++ Raiva/Raiva/View/CreateComunity.swift
@@ -4,16 +4,12 @@
 //
 //  Created by Dan Figueroa on 30/03/25.
 //
-
 import SwiftUI
 
 struct CreateComunity: View {
     @StateObject private var createComunnityData = CreateComunnityViewModel()
     @State private var showImagePicker = false
-    @State private var selectedImage: String = "perfilInvitado"
-    
-    let availableImages = ["chol", "festividadChol", "chuj", "mam","qanjobal","tojolabal","tsotsil","tseltal","zoque", "perfilInvitado"]
-    
+
     var body: some View {
         ZStack {
             Rectangle()
@@ -24,13 +20,13 @@ struct CreateComunity: View {
 
             VStack(spacing: 0) {
                 ZStack {
-                    Image(selectedImage)
+                    Image(createComunnityData.selectedImage)
                         .resizable()
                         .scaledToFill()
                         .frame(height: 180)
                         .offset(y: 80)
                         .blur(radius: 1)
-        
+
                     Rectangle()
                         .foregroundColor(.black.opacity(0.5))
                         .cornerRadius(30)
@@ -41,28 +37,30 @@ struct CreateComunity: View {
                 Spacer()
             }
             .frame(width: 900, height: 700)
-            
+
             Circle()
                 .fill(Color.arena.opacity(0.3))
                 .frame(width: 180, height: 180)
                 .overlay(
                     ZStack {
-                        Image(selectedImage)
+                        Image(createComunnityData.selectedImage)
                             .resizable()
                             .scaledToFill()
                             .frame(width: 160, height: 160)
                             .clipShape(Circle())
-                        
+
                         Circle()
                             .stroke(Color.arena, lineWidth: 3)
-                        
-                        VStack(spacing: 5) {
-                            CustomButton(action: {
-                                showImagePicker.toggle()
-                            }, style: .image(imageName: "camara"))
-                            Text("Agregar foto")
-                                .foregroundColor(.white)
-                                .bold()
+
+                        if createComunnityData.selectedImage == "perfilInvitado" {
+                            VStack(spacing: 5) {
+                                CustomButton(action: {
+                                    showImagePicker.toggle()
+                                }, style: .image(imageName: "camara"))
+                                Text("Agregar foto")
+                                    .foregroundColor(.white)
+                                    .bold()
+                            }
                         }
                     }
                 )
@@ -81,7 +79,7 @@ struct CreateComunity: View {
                     width: 800,
                     borderColor: Color.arena
                 )
-                
+
                 CustomTextField(
                     title: "Descripción",
                     placeholder: "",
@@ -92,16 +90,16 @@ struct CreateComunity: View {
                     width: 800,
                     borderColor: Color.arena
                 )
-                
+
                 CustomButton(action: {}, style: .standard(fontColor: .arena, backgroundColor: .verdeBosque.opacity(0.8), buttonName: "Publicar"))
-                .frame(width: 200)
-                .frame(maxWidth: .infinity, alignment: .trailing)
-                .padding(.trailing, 40)
+                    .frame(width: 200)
+                    .frame(maxWidth: .infinity, alignment: .trailing)
+                    .padding(.trailing, 40)
             }
             .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
             .padding(.leading, 10)
             .padding(.top, 330)
-            
+
             CustomButton(action: {}, style: .image(imageName: "x"))
                 .scaleEffect(0.8)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .topLeading)
@@ -110,49 +108,10 @@ struct CreateComunity: View {
         }
         .frame(width: 900, height: 700)
         .sheet(isPresented: $showImagePicker) {
-            ImagePickerView(selectedImage: $selectedImage, availableImages: availableImages)
+            ImagePickerComponent(selectedImage: $createComunnityData.selectedImage, availableImages: createComunnityData.availableImages)
         }
     }
 }
-struct ImagePickerView: View {
-        @Binding var selectedImage: String
-        let availableImages: [String]
-        @Environment(\.dismiss) var dismiss
-        
-        var body: some View {
-            VStack {
-                Text("Selecciona una foto")
-                    .font(.title)
-                    .padding()
-                
-                ScrollView {
-                    LazyVGrid(columns: [GridItem(.adaptive(minimum: 150))], spacing: 20) {
-                        ForEach(availableImages, id: \.self) { imageName in
-                            Button(action: {
-                                selectedImage = imageName
-                                dismiss()
-                            }) {
-                                Image(imageName)
-                                    .resizable()
-                                    .scaledToFill()
-                                    .frame(width: 150, height: 150)
-                                    .clipShape(Circle())
-                                    .overlay(
-                                        Circle()
-                                            .stroke(selectedImage == imageName ? Color.arena : Color.clear, lineWidth: 4)
-                                    )
-                            }
-                            .buttonStyle(PlainButtonStyle())
-                        }
-                    }
-                    .padding()
-                }
-            }
-            .frame(minWidth: 600, minHeight: 500)
-            .background(Color.verdeBosque.opacity(0.1))
-        }
-    }
-
 #Preview {
     CreateComunity()
 }
